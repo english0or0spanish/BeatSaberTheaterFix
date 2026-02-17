162af3078f0ac681f29013fffffdfbab7af8c8d6
english0or0spanish <charliesmee77@gmail.com>
2026-02-17 23:19:06 +0000

    Update wrapper.ts

--- wrapper.ts
+++ wrapper.ts
@@ -2,10 +2,15 @@ const exeDir = Deno.execPath().replace(/[^/\\]+$/, "");
 const theaterFixDir = exeDir + "..\\TheaterFix\\";
 const realExe = theaterFixDir + "yt-dlp-real.exe";
 const cookiesPath = theaterFixDir + "cookies.txt";
+const denoPath = exeDir + "deno.exe";
 
-const args = Deno.args
-  .filter(a => a !== "--ignore-config")
-  .concat(["--cookies", cookiesPath]);
+let args = Deno.args.filter(a => a !== "--ignore-config");
+
+if (!args.some(a => a.startsWith("--js-runtimes"))) {
+  args = args.concat(["--js-runtimes", `deno:${denoPath}`]);
+}
+
+args = args.concat(["--cookies", cookiesPath]);
 
 const cmd = new Deno.Command(realExe, {
   args,
